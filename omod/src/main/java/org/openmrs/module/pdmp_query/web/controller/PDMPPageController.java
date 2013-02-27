d0a15668581f7344bee9e5388f04eba4411157ea
toby cabot <ccabot@mitre.org>
2013-02-27 17:35:59 -0500

    remove some unused imports

--- omod/src/main/java/org/openmrs/module/pdmp_query/web/controller/PDMPPageController.java
+++ omod/src/main/java/org/openmrs/module/pdmp_query/web/controller/PDMPPageController.java
@@ -1,11 +1,8 @@
 package org.openmrs.module.pdmp_query.web.controller;
 
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
